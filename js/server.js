79289fbc1c4d3aaae236b78379dcfc33d995253f
gleb5783 <gleb5783@mail.ru>
2021-03-13 17:17:56 +0300

    правки линта

--- js/server.js
+++ js/server.js
@@ -16,7 +16,7 @@ fetch('https://22.javascript.pages.academy/kekstagram/data')
     anyUsersCard(imgArray);
   })
   .catch(() => {
-    document.body.innerHTML = "<div class='error'><h1>Error</h1><span>Пожалуйста перезагрузите страницу</span></div>";
+    document.body.innerHTML = '<div class=`error`><h1>Error</h1><span>Пожалуйста перезагрузите страницу</span></div>';
   });
 
 
@@ -31,37 +31,37 @@ imgForm.addEventListener('submit', (evt) => {
       body: formData,
     },
   )
-  .then(() => {
-    closeForm();
-  })
-  .then(() => {
-    closeSuccessButton.addEventListener('click', () => {
-      successTemplate.remove();
-    });
-    document.addEventListener('keydown', (evt) => {
-      if(evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON) {
+    .then(() => {
+      closeForm();
+    })
+    .then(() => {
+      closeSuccessButton.addEventListener('click', () => {
         successTemplate.remove();
-      }
-    });
-    document.addEventListener('click', () => {
-      successTemplate.remove();
-    });
-    main.appendChild(successTemplate);
-  })
-  .then(() => {
-    closeSuccessButton.removeEventListener('click', () => {
-      successTemplate.remove();
-    });
-    document.removeEventListener('keydown', (evt) => {
-      if(evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON) {
+      });
+      document.addEventListener('keydown', (evt) => {
+        if(evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON) {
+          successTemplate.remove();
+        }
+      });
+      document.addEventListener('click', () => {
         successTemplate.remove();
-      }
+      });
+      main.appendChild(successTemplate);
+    })
+    .then(() => {
+      closeSuccessButton.removeEventListener('click', () => {
+        successTemplate.remove();
+      });
+      document.removeEventListener('keydown', (evt) => {
+        if(evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON) {
+          successTemplate.remove();
+        }
+      });
+      document.removeEventListener('click', () => {
+        successTemplate.remove();
+      });
+    })
+    .catch(() => {
+      main.appendChild(errorTemplate);
     });
-    document.removeEventListener('click', () => {
-      successTemplate.remove();
-    });
-  })
-  .catch(() => {
-    main.appendChild(errorTemplate);
-  });
 });
